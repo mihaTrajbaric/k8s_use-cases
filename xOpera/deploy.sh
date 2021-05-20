b9f1763d55825e5e56a9e9e5c720a9d2b8294f10
mihaTrajbaric <26118837+mihaTrajbaric@users.noreply.github.com>
2021-05-20 12:36:18 +0200

    Add tls

--- xOpera/deploy.sh
+++ xOpera/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kubectl apply -f k8s-config/config.yaml
-kubectl apply -f k8s-config/postgres.yaml
-kubectl apply -f k8s-config/xopera.yaml
+kubectl apply -f k8s_manifests/config.yaml
+kubectl apply -f k8s_manifests/postgres.yaml
+kubectl apply -f k8s_manifests/xopera.yaml
